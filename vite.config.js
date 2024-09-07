4e0175ffa7ccf3b22602b678ad54afbdb87ccc53
Vijay <155041091+vijayjadhav75@users.noreply.github.com>
2024-09-07 11:26:25 +0530

    url change

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'production' ? '/personal_website/' : '/',
-    build: {
-      outDir: './docs'
-    }
+export default defineConfig({
+  plugins: [react()],
+  base:"/personal_website/",
+  build:{
+    outDir:'./docs'
   }
 })
